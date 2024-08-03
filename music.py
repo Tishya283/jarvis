b0e90f5a92c759efa3f81968dd89df13fa357f63
Tishya283 <144004638+Tishya283@users.noreply.github.com>
2024-08-03 18:47:15 +0530

    music.py

--- music.py
+++ music.py
@@ -1,3 +1,4 @@
+# initialise your command and the urls which you want to open in the following dictionary
 music = {
     "tamatar":"https://www.youtube.com/watch?v=_oWY20C1lvA",
     "oh maahi":"https://www.youtube.com/watch?v=Zlqf9cuaOBw",
